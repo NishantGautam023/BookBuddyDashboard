745e09b97d5ce9e9cfe4ec0016e291b499bf2626
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-06-12 15:38:50 -0400

    Add Tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {router} from "../router.tsx";
 import {RouterProvider} from "react-router-dom";
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+
+
       <RouterProvider router={router}>
 
       </RouterProvider>
+
+      </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,8 +10,22 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {Link} from "react-router-dom";
+import {useRef} from "react";
+
 
 export default function LoginPage() {
+        const emailRef = useRef<HTMLInputElement>(null);
+        const passwordRef = useRef<HTMLInputElement>(null);
+
+        function onHandleSubmitClick() {
+                const email = emailRef.current?.value
+                const password = passwordRef.current?.value
+
+                // Make server call
+                console.log(`Data we got is Email: ${email} and Password is ${password}` )
+
+        }
+
     return (
         <section className="flex justify-center items-center h-screen">
 
@@ -25,19 +39,19 @@ export default function LoginPage() {
                 <CardContent className="grid gap-4">
                     <div className="grid gap-2">
                         <Label htmlFor="email">Email</Label>
-                        <Input id="email" type="email" placeholder="m@example.com" required/>
+                        <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required/>
                     </div>
                     <div className="grid gap-2">
                         <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" required/>
+                        <Input ref={passwordRef} id="password" type="password" required/>
                     </div>
                 </CardContent>
                 <CardFooter>
-                    <Button className="w-full">Sign in</Button>
+                    <Button onClick={onHandleSubmitClick} className="w-full">Sign in</Button>
                 </CardFooter>
                 <div className="mt-4 text-center text-sm pb-8">
                     New to BookBuddy?{" "}
-                    <Link to={'/register'} className="underline">
+                    <Link to={'/auth/register'} className="underline">
                         Register
                     </Link>
                 </div>
