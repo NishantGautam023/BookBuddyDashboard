e2d2b79323c709e11e18d4a10e721eaf2361e4f1
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-06-12 14:59:24 -0400

    Add Auth Layout

--- router.tsx
+++ router.tsx
@@ -2,12 +2,41 @@ import LoginPage from "./src/pages/LoginPage";
 import HomePage from "./src/pages/HomePage";
 import RegisterPage from "@/pages/RegisterPage.tsx";
 import {createBrowserRouter} from "react-router-dom";
+import DashboardLayout from "@/layouts/DashboardLayout.tsx";
+import BooksPage from "@/pages/BooksPage.tsx";
+import AuthLayout from "@/layouts/AuthLayout.tsx";
 
 // @ts-ignore
 export const router = new createBrowserRouter([
     {
-        path: "/",
-        element: <HomePage />
+        path: "dashboard",
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+            {
+                path: 'books',
+                element: <BooksPage />,
+            }
+        ]
+
+    },
+
+    {
+      path: '/auth',
+      element: <AuthLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <LoginPage />,
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />,
+            }
+        ]
     },
 
     {
---
+++ src/pages/BooksPage.tsx
@@ -0,0 +1,7 @@
+export default function BooksPage() {
+    return (
+        <>
+            <h1>THis is a BooksPage</h1>
+        </>
+    )
+}
