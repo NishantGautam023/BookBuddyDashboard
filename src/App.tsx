c50725b8325a5a7dac07b26ebb0452101c62a29d
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-06-12 07:59:38 -0400

    Add SHADCN UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 
 
-import './App.css'
+import './index.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
 
 
       <h1>Welcome to BookBuddy Dashboard</h1>
+        <Button variant="destructive">Button</Button>
+
     </>
   )
 }
