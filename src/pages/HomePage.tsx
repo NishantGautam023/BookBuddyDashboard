06ca349c3358a3832db4ca55f6866eac79b57fe0
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-07-07 23:15:36 -0400

    Added toast notifaction and fixed login and auth validation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import {Button} from "@/components/ui/button.tsx";
 import {Link} from "react-router-dom";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table.tsx";
 import {Badge} from "@/components/ui/badge.tsx";
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar.tsx";
+import { ChartPage } from "./ChartPage";
 
 
 export default function HomePage() {
@@ -12,6 +12,7 @@ export default function HomePage() {
         <>
             <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
                 <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+                    {/* -------Revenue box section---------- */}
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                             <CardTitle className="text-sm font-medium">
@@ -63,9 +64,13 @@ export default function HomePage() {
                                 +201 since last hour
                             </p>
                         </CardContent>
+                       
                     </Card>
                 </div>
+
+     
                 <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+                    {/* ------Transcation section ----------- */}
                     <Card
                         className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
                     >
@@ -204,88 +209,17 @@ export default function HomePage() {
                                 </TableBody>
                             </Table>
                         </CardContent>
+                        
                     </Card>
+
+
+
+                    {/* --------Recent Sales section---------- */}
                     <Card x-chunk="dashboard-01-chunk-5">
                         <CardHeader>
                             <CardTitle>Recent Sales</CardTitle>
                         </CardHeader>
-                        <CardContent className="grid gap-8">
-                            <div className="flex items-center gap-4">
-                                <Avatar className="hidden h-9 w-9 sm:flex">
-                                    <AvatarImage src="/avatars/01.png" alt="Avatar"/>
-                                    <AvatarFallback>OM</AvatarFallback>
-                                </Avatar>
-                                <div className="grid gap-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        Olivia Martin
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        olivia.martin@email.com
-                                    </p>
-                                </div>
-                                <div className="ml-auto font-medium">+$1,999.00</div>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <Avatar className="hidden h-9 w-9 sm:flex">
-                                    <AvatarImage src="/avatars/02.png" alt="Avatar"/>
-                                    <AvatarFallback>JL</AvatarFallback>
-                                </Avatar>
-                                <div className="grid gap-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        Jackson Lee
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        jackson.lee@email.com
-                                    </p>
-                                </div>
-                                <div className="ml-auto font-medium">+$39.00</div>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <Avatar className="hidden h-9 w-9 sm:flex">
-                                    <AvatarImage src="/avatars/03.png" alt="Avatar"/>
-                                    <AvatarFallback>IN</AvatarFallback>
-                                </Avatar>
-                                <div className="grid gap-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        Isabella Nguyen
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        isabella.nguyen@email.com
-                                    </p>
-                                </div>
-                                <div className="ml-auto font-medium">+$299.00</div>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <Avatar className="hidden h-9 w-9 sm:flex">
-                                    <AvatarImage src="/avatars/04.png" alt="Avatar"/>
-                                    <AvatarFallback>WK</AvatarFallback>
-                                </Avatar>
-                                <div className="grid gap-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        William Kim
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        will@email.com
-                                    </p>
-                                </div>
-                                <div className="ml-auto font-medium">+$99.00</div>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <Avatar className="hidden h-9 w-9 sm:flex">
-                                    <AvatarImage src="/avatars/05.png" alt="Avatar"/>
-                                    <AvatarFallback>SD</AvatarFallback>
-                                </Avatar>
-                                <div className="grid gap-1">
-                                    <p className="text-sm font-medium leading-none">
-                                        Sofia Davis
-                                    </p>
-                                    <p className="text-sm text-muted-foreground">
-                                        sofia.davis@email.com
-                                    </p>
-                                </div>
-                                <div className="ml-auto font-medium">+$39.00</div>
-                            </div>
-                        </CardContent>
+                         <ChartPage/>
                     </Card>
                 </div>
             </main>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,6 +15,12 @@ import {useMutation} from "@tanstack/react-query";
 import {login} from "@/http/api.ts";
 import {Loader} from "lucide-react";
 import useTokenStore from "@/store.ts";
+import { useToast } from "@/components/ui/use-toast";
+
+
+
+
+
 
 
 export default function LoginPage() {
@@ -25,6 +31,15 @@ export default function LoginPage() {
         const emailRef = useRef<HTMLInputElement>(null);
         const passwordRef = useRef<HTMLInputElement>(null);
 
+    const { toast } = useToast()
+    
+
+      const isValidEmail = (email: string) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+ 
+  
 
     // Mutations
     const mutation = useMutation({
@@ -33,17 +48,53 @@ export default function LoginPage() {
 
                 console.log("Login successful", response)
             setToken(response.data.accessToken);
+            toast({
+                description: "Login Successful!",
+                duration: 1500,
+               variant: "destructive"
 
+
+
+           })
             navigate('/dashboard/home')
+       
 
         },
+        onError: (error: any) => {
+            let errorMessage = "An error occurred while logging in. Please try again.";
+
+            if (error.response) {
+                if (error.response.status === 404) {
+                    errorMessage = "The requested email or password was not found.";
+                } else if (error.response.status === 500) {
+                    errorMessage = "Internal server error. Please try again later.";
+                }
+            } else if (error.request) {
+                 toast({
+                description: errorMessage,
+                duration: 1500,
+               variant: "destructive"
+           })
+                return;
+            }
+
+              toast({
+                description: errorMessage,
+                duration: 1500,
+               variant: "destructive"
+           })
+        }
+       
+
+        
     })
 
-        function onHandleSubmitClick() {
+    function onHandleSubmitClick(event: React.FormEvent) {
+                event.preventDefault()
                 const email = emailRef.current?.value
                 const password = passwordRef.current?.value
 
-                // Make server call
+              
 
 
 
@@ -51,7 +102,9 @@ export default function LoginPage() {
             if(!email || !password) {
                 return alert("Please enter your email and password")
             }
-
+            
+        
+        
             mutation.mutate({email, password})
 
 
@@ -62,21 +115,24 @@ export default function LoginPage() {
         <section className="flex justify-center items-center h-screen">
 
             <Card className="w-full max-w-sm">
+                <form onSubmit={onHandleSubmitClick}> 
+
+                
                 <CardHeader>
                     <CardTitle className="text-2xl">Login</CardTitle>
                     <CardDescription>
                         Enter your email below to login to your account.<br/>
-                        {mutation.isError && <span className="text-red-500 text-sm">{mutation.error.message}</span>}
+                        {mutation.isError && <span className="text-red-500 text-sm">Please Enter correct Email or Password</span>}
                     </CardDescription>
                 </CardHeader>
                 <CardContent className="grid gap-4">
                     <div className="grid gap-2">
                         <Label htmlFor="email">Email</Label>
-                        <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required/>
+                        <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" autoComplete="email" required/>
                     </div>
                     <div className="grid gap-2">
                         <Label htmlFor="password">Password</Label>
-                        <Input ref={passwordRef} id="password" type="password" required/>
+                        <Input ref={passwordRef} id="password" type="password" autoComplete="password" required/>
                     </div>
                 </CardContent>
                 <CardFooter>
@@ -85,18 +141,21 @@ export default function LoginPage() {
                             Loading ...  <Loader className="animate-spin" />
                         </Button>
                     ) : (
-                        <Button onClick={onHandleSubmitClick} className="w-full">Sign in</Button>
+                        <Button type="submit" className="w-full">Sign in</Button>
                     )}
 
 
-                </CardFooter>
+                    </CardFooter>
+                    </form>
                 <div className="mt-4 text-center text-sm pb-8">
                     New to BookBuddy?{" "}
                     <Link to={'/auth/register'} className="underline">
                         Register
                     </Link>
                 </div>
+            
             </Card>
+          
         </section>
 
     )
